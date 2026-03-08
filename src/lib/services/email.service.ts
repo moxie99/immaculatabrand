b63968f8c1d06cbfd4098b57c0f608a6808b1a53
Adeolu Oluwasegun <adeolusegun1000@gmail.com>
2026-03-08 22:20:28 +0100

    feat: fixed the email service workaround

--- src/lib/services/email.service.ts
+++ src/lib/services/email.service.ts
@@ -32,9 +32,7 @@ interface OrderWithId {
 }
 
 const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: parseInt(process.env.EMAIL_PORT || '587'),
-  secure: process.env.EMAIL_SECURE === 'true',
+  service: 'gmail',
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
